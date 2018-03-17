68f0dc8a94e87e4eaf81e7b2f472c8b8472322b2
Student <student@Admins-MacBook-Air.local>
2018-03-17 13:01:53 -0400

    Step 6

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-/* remove this comment and place your JavaScript here */ 
+var friend = null;
+var number = null;
+var hobbies = null;
+var zodiac = null;
+var color = null;
+var bias = null;
+
+var pageTitle = document.getElementById("pageTitle");
+var pageTitleText = pageTitle.innerHTML;
+var tryAgain = document.getElementById("try-again");
+var quizWrapper = document.getElementById("quizWrapper");
+var result = document.getElementById("result");
+var formSubmit = document.getElementById("formSubmit");
+
+tryAgain.addEventListener("click", resetQuiz);
+formSubmit.addEventListener("click", processResults);
+
+function resetQuiz() {
+
+}
+
+function processResults() {
+
+}
+
+function getPersonality() {
+	var score = 0;
+}
